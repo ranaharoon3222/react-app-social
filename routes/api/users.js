3772c7e3459433d1b8a03b82e48bb8390f5aca81
Rana Haroon <ranaharoon3222@gmail.com>
2020-01-10 09:16:00 +0500

    initial ROutes user/post/auth/profile

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Get ROute api/
+// main Route
+// Public route
+
+router.get('/', (req,res)=>{
+    res.send('User Route');
+})
+
+module.exports = router;
