ef82ade4c66933eb1808271eb9231a96b5c3d5af
Rana Haroon <ranaharoon3222@gmail.com>
2020-01-11 11:55:11 +0500

    Profile model and route created successfuly

---
+++ models/profile.js
@@ -0,0 +1,113 @@
+const mongoose = require('mongoose');
+
+const Profileschema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experiences: [
+    {
+      company: {
+        type: String
+      },
+      title: {
+        type: String
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', Profileschema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Profile = require('../../models/User');
+const Profile = require('../../models/profile');
 
-// Get ROute api/
-// main Route
+// Get ROute api/profile/me
+// @desc Get Current User Profile
 // Public route
 
-router.get('/', (req,res)=>{
-    res.send('Profile Route');
-})
+router.get('/', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: rq.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile) {
+      res.status(400).json({ msg: 'There is no profile for this user ' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
