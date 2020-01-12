99679885b4f2b9b555f4f44a145b2baa84849abe
Rana Haroon <ranaharoon3222@gmail.com>
2020-01-12 17:11:15 +0500

    add like post feature

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -30,7 +30,9 @@ router.post(
     }
 
     try {
-      const user = await (await User.findById(req.user.id)).isSelected('-password');
+      const user = await (await User.findById(req.user.id)).isSelected(
+        '-password'
+      );
 
       const newPost = new Post({
         text: req.body.text,
@@ -46,5 +48,88 @@ router.post(
     }
   }
 );
+
+// Post  api/posts
+// GET all Post
+// @access private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// Post  api/posts/:id
+// GET  Post by id
+// @access private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// Post  api/posts/:id
+// Delete  Post by id
+// @access private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+    //check User
+    if (post.user.toString() !== req.user.id) {
+      return res.status(404).json({ msg: 'User Not authorized' });
+    }
+    await post.remove();
+    res.json({ msg: 'Post Removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// PUT  api/posts/like/:id
+// Delete  Post by id
+// @access private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'User already liked this post' });
+    }
+    
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not Found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
