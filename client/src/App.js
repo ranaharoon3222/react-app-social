a32a6f057c77f4d610a1d59c1928b02597f4c094
Rana Haroon <ranaharoon3222@gmail.com>
2020-01-14 17:18:06 +0500

    React v2

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/login';
 import Register from './components/auth/register';
 import './App.css';
 
+
 const App = () => (
   <Router>
     <Fragment>
--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import axios from 'axios';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const {  email, password } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+      console.log('Success')
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password
+      // };
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   };
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('/api/users', body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.error(err.response.data);
+      // }
+    
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Fragment>
+      <h1 className='large text-primary'>Log In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Login    </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='3'
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Not have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
   );
 };
 
