56468bcddc5dd4fb517d5161f01279fe13c45b53
Rana Haroon <ranaharoon3222@gmail.com>
2020-03-04 21:19:10 +0500

    Finsih Profile Action & Reducers

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  GET_REPOS
 } from '../actions/types';
 
 const initialState = {
@@ -24,6 +26,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -31,12 +39,17 @@ export default function(state = initialState, action) {
         loading: false
       };
     case CLEAR_PROFILE:
-  
       return {
         ...state,
         profile: null,
         loading: false,
         repos: []
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
       };
     default:
       return state;
