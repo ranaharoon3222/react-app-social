b031e6158d7980d2f3ce35de023c1496247f325e
Rana Haroon <ranaharoon3222@gmail.com>
2020-03-05 01:34:38 +0500

    display profile and use schmoe

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,8 +7,12 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
+
       <li>
         <Link to='/dashboard'> <i className='fas fa-user'></i> Dashboard </Link>
+      </li>
+      <li>
+        <Link to='/profiles'>Developers </Link>
       </li>
       <li>
         <a onClick={logout} href='#!'>
@@ -21,7 +25,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers </a>
+        <Link to='/profiles'>Developers </Link>
       </li>
       <li>
         <Link to='/register'>Register </Link>
